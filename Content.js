1dbcde100cec9bc5405f07f27d4145125f0c3a60
unknown <mohamedelkholti062@gmail.com>
2021-12-07 22:47:48 +0100

    append adds in google search

--- Content.js
+++ Content.js
@@ -2,7 +2,15 @@
 //create a div with some content
 function generateDiv() {
     let div = document.createElement("div");
-    div.innerHTML="<h3>Hello world</h3>";
+    div.innerHTML = " <div>"
+        + "<strong>Annonce </strong><a href=''>https://www.media.com/</a>"
+        + "<h3 style='color:#2c2c8d'>Hotel Paris Opera Affiliated By Melia</h3>"
+        + "<p>"
+        + "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+        + "Lorem Ipsum has been the industry's"
+        + "</p>"
+
+    "</div>";
     return div;
 }
 //getting the id of the div containing all the search content
