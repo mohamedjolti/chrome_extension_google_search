71ee41001224fe236152ebb24b5750e2dc589158
unknown <mohamedelkholti062@gmail.com>
2021-12-08 19:40:40 +0100

    verify that div containing search result exist

--- Content.js
+++ Content.js
@@ -1,32 +1,32 @@
 
 // create  Ads with the title ,the url and the description 
 function generateAds(url, title, description) {
-    let div = document.createElement("div");
-    div.innerHTML = `<div class='taw'>
-                       <div class='uEierd'>
-                         <strong>Annonce </strong><a href='${url}'>${url}</a>
-                         <a href='${url}'>
-                           <h3 style='color:#2c2c8d;cursor: pointer'>${title}</h3>
-                         </a>
-                         <p class='VwiC3b yXK7lf MUxGbd yDYNvb lyLwlc lEBKkf'>      
-                            ${description}   
-                         </p>
-                         
-                        </div>
-                      </div><br>`;
-    return div;
+  let div = document.createElement("div");
+  div.innerHTML = `<div class='uEierd'>
+                       <strong>Annonce </strong><a href='${url}'>${url}</a>
+                       <a href='${url}'>
+                         <h3 style='color:#2c2c8d;cursor: pointer'>${title}</h3>
+                       </a>
+                       <p class='VwiC3b yXK7lf MUxGbd yDYNvb lyLwlc lEBKkf'>      
+                          ${description}   
+                       </p>
+                       
+                      </div>
+                     <br>`;
+  return div;
 }
 
 //getting the id of the div containing all the search content
-var googleSearchDiv = document.getElementById("search");
+var googleSearchDiv = document.getElementById("taw");
 
-//append the ads to google search 
-googleSearchDiv.prepend(
-    generateAds("https://www.booking.com/", "Hotel Paris - France -Booking.com",
-        "Réservé votre last minute online..."));
+//prpend the ads to google search 
+if(googleSearchDiv != null){
+    googleSearchDiv.prepend(
+      generateAds("https://www.booking.com/", "Hotel Paris - France -Booking.com",
+          "Réservé votre last minute online..."));
 
-googleSearchDiv.prepend(
-    generateAds("https://www.media.com/", "Hotel Paris Opera Affiliated By Melia",
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry."));
-
+    googleSearchDiv.prepend(
+      generateAds("https://www.media.com/", "Hotel Paris Opera Affiliated By Melia",
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry."));
+}
 
