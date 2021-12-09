378987c2f275718d9cb84dafb2dd7cb99c751259
unknown <mohamedelkholti062@gmail.com>
2021-12-09 17:22:28 +0100

    display the custom ads in every google search page

--- Content.js
+++ Content.js
@@ -5,7 +5,7 @@ function generateAds() {
     var personalizedAds = document.createElement("div");
     personalizedAds.innerHTML = html;
     //get the div containing the Ads from the google result page
-    var googleAdsDiv = document.getElementById("tads");
+    var googleAdsDiv = document.getElementById("taw");
     //add the ads to the page
     if(googleAdsDiv != null)
       googleAdsDiv.prepend(personalizedAds);
