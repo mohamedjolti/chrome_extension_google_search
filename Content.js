a6518eab12c8d5fd6e30b7faee8b8f9300218fd4
unknown <mohamedelkholti062@gmail.com>
2021-12-13 22:45:08 +0100

    verify code standars using JSlint

--- Content.js
+++ Content.js
@@ -1,16 +1,16 @@
-
 function generateAds() {
-  //fetch html content from template file
-  fetch(chrome.runtime.getURL('/template.html')).then(r => r.text()).then(html => {
-    var personalizedAds = document.createElement("div");
-    personalizedAds.innerHTML = html;
-    //get the div containing the Ads from the google result page
-    var googleAdsDiv = document.getElementById("taw");
-    //add the ads to the page
-    if(googleAdsDiv != null)
-      googleAdsDiv.prepend(personalizedAds);
-    
-  });
+  //get the div containing the Ads from the google result page
+  var googleAdsDiv = document.getElementById("taw");
+  var personalizedAds;
+  if (googleAdsDiv !== null) {
+    //fetch html content from template file
+    fetch(chrome.runtime.getURL("/template.html")).
+      then((r) => r.text()).then((html) => {
+        personalizedAds= document.createElement("div");
+        personalizedAds.innerHTML = html;
+        //add the ads to the page
+        googleAdsDiv.prepend(personalizedAds);
+      });
+  }
 }
-
 generateAds();
